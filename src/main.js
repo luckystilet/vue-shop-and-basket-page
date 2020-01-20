389dba6e141db9c2b2733244efc6e95ea2a313eb
Alex <luckystilet@gmail.com>
2020-01-20 07:09:33 +0200

    'ok'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/'
-import router from './router'
 
 import './css/reload-styles.css'
 import './css/style.css'
@@ -11,7 +10,6 @@ import './css/media.css'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App),
 }).$mount('#app')
